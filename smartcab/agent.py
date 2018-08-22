9e8d902ae5f8a48779c2cc30360485034b33b90b
Jonasz Karwacki <jonaszakr@gmail.com>
2018-08-21 19:38:38 -0700

    Update hyperparams

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -164,7 +164,10 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env)
+    sim = Simulator(env,
+                    update_delay=2.,
+                    log_metrics=True,
+                    optimized=True)
 
     ##############
     # Run the simulator
